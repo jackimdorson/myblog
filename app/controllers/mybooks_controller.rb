2dfd44a7827ec0678488f53a2cfde6bb045a5118
Yoshiyuki Sugimoto <s.yosiyuki@gmail.com>
2018-07-31 22:08:55 +0700

    :recycle: fix indent

--- app/controllers/mybooks_controller.rb
+++ app/controllers/mybooks_controller.rb
@@ -1,31 +1,26 @@
 class MybooksController < ApplicationController
+  layout 'mybooks'
 
-  layout = 'mybooks'
-
-def index
-    page_size = 5  
-    @page_num = 0  
-     if params[:page] != nil then
-        @page_num = params[:page].to_i
-     end
-     @mypost = Mypost.all.order('created_at desc')
-     .offset(page_size * @page_num)
-     .limit(page_size)
-end
-
-def genre
-     page_size = 5
-     @page_num = 0
+  def index
+    page_size = 5
+    @page_num = 0
     if params[:page] != nil then
-         @page_num = params[:page].to_i
+      @page_num = params[:page].to_i
     end
-     @mygenre = Mygenre.find params[:id]
-     @mypost = Mypost.where('genre_id = ?', params[:id])
-     .order('created_at desc').offset(page_size * @page_num)
-     .limit(page_size)
-end
+    @mypost = Mypost.all.order('created_at desc').offset(page_size * @page_num).limit(page_size)
+  end
 
-def show
+  def genre
+    page_size = 5
+    @page_num = 0
+    if params[:page] != nil then
+       @page_num = params[:page].to_i
+    end
+    @mygenre = Mygenre.find params[:id]
+    @mypost = Mypost.where('genre_id = ?', params[:id]).order('created_at desc').offset(page_size * @page_num).limit(page_size)
+  end
+
+  def show
     @mypost = Mypost.find params[:id]
-end
+  end
 end
